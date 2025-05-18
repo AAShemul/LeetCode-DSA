955673b3463c911e40d31d49b36dfd41290e3a9d
Md. Ashraful Alam Shemul <aashemul@gmail.com>
2025-05-19 01:42:31 +0600

    feat(1-two-sum): add JS and Python solution

--- src/1-100/1. Two Sum/Solution.js
+++ src/1-100/1. Two Sum/Solution.js
@@ -1,10 +1,30 @@
-// Less Efficient Solution
+// More Efficient Solution
 /**
  * @param { number[] } nums
  * @param { number } target
  * @return { number[] }
  */
 const twoSum = function (nums, target) {
+	const numsMap = new Map()
+
+	for (let i = 0; i < nums.length; i++) {
+		let comp = target - nums[i]
+
+		if (numsMap.has(comp)) return [ numsMap.get(comp), i ]
+		numsMap.set(nums[i], i)
+	}
+
+	return []
+}
+
+
+// Less Efficient Solution
+/**
+ * @param { number[] } nums
+ * @param { number } target
+ * @return { number[] }
+ */
+const twoSum2 = function (nums, target) {
 	for (let i = 0; i < nums.length - 1; i++) {
 		for (let j = i + 1; j < nums.length; j++) {
 			if (i !== j && nums[i] + nums[j] === target) {
--- src/1-100/1. Two Sum/Solution.py
+++ src/1-100/1. Two Sum/Solution.py
@@ -1,8 +1,25 @@
 from typing import List
 
 
-# Less Efficient Solution
+# More Efficient Solution
 class Solution:
+    @staticmethod
+    def twoSum(nums: List[int], target: int) -> List[int]:
+        numsMap = {}
+
+        for i in range(len(nums)):
+            comp = target - nums[i]
+
+            if comp in numsMap:
+                return [numsMap[comp], i]
+
+            numsMap[nums[i]] = i
+
+        return []
+
+
+# Less Efficient Solution
+class Solution2:
     @staticmethod
     def twoSum(nums: List[int], target: int) -> List[int]:
         for i in range(len(nums) - 1):
